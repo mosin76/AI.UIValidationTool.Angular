e6193d9f362b342f3b9b2786b179c47acc228e3f
mosin <mosin76@gmail.com>
2024-11-06 23:05:45 +0530

    commited the document download look & feel

--- src/app/review-doc/review-doc.component.ts
+++ src/app/review-doc/review-doc.component.ts
@@ -38,6 +38,7 @@ export class ReviewDocComponent extends AutoSquaredBaseComponent {
     zoomLevel: number = 1;
     zoomFactor = 1;  // Initial zoom factor
     borderSize = 2;
+    fileextension :string ='';
     borderColor = '#000000';
     imageTransform: string = '';
     positionX: number = 0;
@@ -181,6 +182,7 @@ export class ReviewDocComponent extends AutoSquaredBaseComponent {
         var tenantId: string| null = this.isTenantUser ? null : this.tenant.id;
         var currentDocId: number = this.currentDocument == null ? -1 : this.currentDocument.id;
         this.reviewService.downloadDocumentMime(tenantId, currentDocId).subscribe(data => {
+            this.fileextension=data.extension;
             this.isImage=this.reviewService.isImageDoc(data.extension)
         });
     }
