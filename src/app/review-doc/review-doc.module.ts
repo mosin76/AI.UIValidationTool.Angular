57fcdb49afd703407327d2053ad4f2cd0f6c57c1
Vishnu Enjapoori <vishnu.enjapoori@saroeinc.com>
2024-11-06 21:54:48 -0500

    Fixed missing component import for the image viewer

--- src/app/review-doc/review-doc.module.ts
+++ src/app/review-doc/review-doc.module.ts
@@ -4,13 +4,13 @@ import { ReviewDocRoutingModule } from './review-doc-routing.module';
 import { ReviewDocComponent } from './review-doc.component';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { NgbPopoverModule, NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
-//import { ImageViewerModule } from 'ng-image-viewer';
 import { NgxImageZoomModule } from 'ngx-image-zoom';
+import { ImageViewerComponent } from '../image-viewer/image-viewer.component'; 
 
 
 @NgModule({
     declarations: [ReviewDocComponent],
     imports: [SharedModule, ReviewDocRoutingModule,NgxImageZoomModule,
-        NgxExtendedPdfViewerModule, NgbPopoverModule, NgbProgressbarModule],
+        NgxExtendedPdfViewerModule, NgbPopoverModule, NgbProgressbarModule, ImageViewerComponent],
 })
 export class ReviewDocModule { }
